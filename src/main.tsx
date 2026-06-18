26637717316c3266ee08f35b3721c809e0a5102a
ProfAntonioCRG <163427510+ProfAntonioCRG@users.noreply.github.com>
2026-06-18 19:32:04 -0300

    Fix GitHub Pages navigation

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 // Entry point - monta a aplicação React no DOM
+// HashRouter é necessário para funcionar em servidores estáticos (GitHub Pages)
+// pois o BrowserRouter requer redirecionamento de servidor para rotas SPA
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
