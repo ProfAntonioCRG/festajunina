4f310419ca812c1343e5cd83fc43cd6d9b7444f4
ProfAntonioCRG <163427510+ProfAntonioCRG@users.noreply.github.com>
2026-06-21 23:24:05 -0300

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/festajunina/',
+  base: '',
   server: {
     host: true,
     port: 3000,
